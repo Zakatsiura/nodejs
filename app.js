dfe6f2f01bb9f3c3b545b7130a301c6876317548
Zakatsiura Ihor <zakatsiura@gmail.com>
2023-05-31 21:29:01 +0300

    init

--- app.js
+++ app.js
@@ -1,9 +1,42 @@
-const event = require('node:events');
+// const event = require('node:events');
+//
+// const eventEmitter = new event();
+//
+// eventEmitter.on('click', () => {
+//     console.log('Click click click');
+// })
+//
+// eventEmitter.emit('click')
 
-const eventEmitter = new event();
 
-eventEmitter.on('click', () => {
-    console.log('Click click click');
+
+// const fs = require('fs');
+// const path = require('path');
+//
+// const readStream = fs.createReadStream('text.txt');
+//
+// readStream.on('data', chunk => {
+//     console.log(chunk);
+// })
+
+
+
+const express = require ('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        name: 'Alex',
+        age: 20,
+        gender: 'male',
+    })
+
+    console.log('Hello')
+});
+
+const PORT = 5001;
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`)
 })
 
-eventEmitter.emit('click')
